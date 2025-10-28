eec4a8a17ddac2c32adaeabb133eaeb0bc4a8b31
Martyneqq <86741262+Martyneqq@users.noreply.github.com>
2025-10-28 20:51:46 +0100

    update

--- SCS_test/main.cpp
+++ SCS_test/main.cpp
@@ -4,12 +4,11 @@
 
 #define MAX_QUEUES 64
 #define MEMORY_SIZE 2048
-#define PAGE_SIZE 135
+#define PAGE_SIZE 8
 
 struct SubQueue {
 	std::uint16_t start_id;
 	std::uint16_t size;
-	std::uint16_t max_size;
 };
 
 typedef std::uint16_t Q;
@@ -42,15 +41,14 @@ Q* create_queue() {
 	SubQueue* sq_ptr = reinterpret_cast<SubQueue*>(&data[sq_byte_index]);
 	sq_ptr->start_id = DATA_POOL_START + (queue_counter * PAGE_SIZE);
 	sq_ptr->size = 0;
-	sq_ptr->max_size = PAGE_SIZE;
 
-	if (sq_ptr->start_id + sq_ptr->max_size > MEMORY_SIZE) {
+	if (sq_ptr->start_id + PAGE_SIZE > MEMORY_SIZE) {
 		on_out_of_memory();
 		queue_counter--;
 		return nullptr;
 	}
 
-	//std::cout << "Sub queue created at: " << sq_ptr << "\nsize: " << sq_ptr->size << std::endl;
+	std::cout << "Sub queue created at: " << sq_ptr << std::endl;
 
 	queue_counter++;
 
@@ -62,21 +60,19 @@ void destroy_queue(Q* q) {
 
 	sq_ptr->start_id = 0;
 	sq_ptr->size = 0;
-	sq_ptr->max_size = 0;
-	queue_counter--;
 }
 // Adds a new byte to a queue.
 void enqueue_byte(Q* q, unsigned char b) {
 	SubQueue* sq_ptr = reinterpret_cast<SubQueue*>(q);
 
-	if (sq_ptr->size >= sq_ptr->max_size) {
+	if (sq_ptr->size >= PAGE_SIZE) {
 		on_out_of_memory();
 	}
 	if (sq_ptr->start_id >= MEMORY_SIZE) {
 		on_out_of_memory();
 	}
 
-	std::uint16_t relative_offset = sq_ptr->size % sq_ptr->max_size;
+	std::uint16_t relative_offset = sq_ptr->size % PAGE_SIZE;
 	std::uint16_t absolute_index = sq_ptr->start_id + relative_offset;
 
 	if (absolute_index >= MEMORY_SIZE) {
@@ -88,7 +84,7 @@ void enqueue_byte(Q* q, unsigned char b) {
 
 	sq_ptr->size++;
 
-	//std::cout << "Enqueued '" << (int)b << "' into queue at " << absolute_index << ". New size: " << sq_ptr->size << std::endl;
+	std::cout << "Enqueued '" << (int)b << "' into queue at " << absolute_index << ". New size: " << sq_ptr->size << std::endl;
 }
 // Pops the next byte off the FIFO queue.
 unsigned char dequeue_byte(Q* q) {
@@ -100,9 +96,8 @@ unsigned char dequeue_byte(Q* q) {
 	}
 
 	unsigned char dequeued_byte = data[sq_ptr->start_id];
-	size_t partition_start = DATA_POOL_START + ((sq_ptr - reinterpret_cast<SubQueue*>(data)) * PAGE_SIZE);
 
-	sq_ptr->start_id = partition_start + ((sq_ptr->start_id - partition_start + 1) % sq_ptr->max_size);
+	sq_ptr->start_id++;
 	sq_ptr->size--;
 
 	return dequeued_byte;
@@ -110,7 +105,26 @@ unsigned char dequeue_byte(Q* q) {
 
 int main()
 {	
-	Q* q0 = create_queue();
+	
+
+	for (size_t i = 0; i < 63; i++)
+	{
+		Q* q = create_queue();
+		for (size_t j = 0; j < 12; j++)
+		{
+			enqueue_byte(q, j);
+		}
+		for (size_t j = 0; j < 12; j++)
+		{
+			printf("%d", dequeue_byte(q));
+			printf("\n");
+		}
+		if (i == 12) {
+			destroy_queue(q);
+		}
+	}
+
+	/*Q* q0 = create_queue();
 	enqueue_byte(q0, 0);
 	enqueue_byte(q0, 1);
 	Q* q1 = create_queue();
@@ -127,5 +141,5 @@ int main()
 	printf("%d", dequeue_byte(q1));
 	printf("%d", dequeue_byte(q1));
 	printf("%d\n", dequeue_byte(q1));
-	destroy_queue(q1);
+	destroy_queue(q1);*/
 }
