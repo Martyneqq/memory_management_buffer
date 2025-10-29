8685f780b2e65f5ed6ad0609ff270951027df36c
Martyneqq <86741262+Martyneqq@users.noreply.github.com>
2025-10-29 16:00:08 +0100

    edited enqueue and dequeue functions for correct function of the linked list

--- SCS_test/main.cpp
+++ SCS_test/main.cpp
@@ -11,10 +11,13 @@ struct Queue {
 	std::uint16_t head;
 	std::uint16_t tail;
 	std::uint16_t size;
+	std::uint8_t read_offset;
 };
 
+const size_t DATA_SIZE = CHUNK_SIZE - sizeof(std::uint16_t);
+
 struct DataChunk {
-	unsigned char data[6];
+	unsigned char data[DATA_SIZE];
 	std::uint16_t next;
 };
 
@@ -35,11 +38,11 @@ void init_chunks() {
 
 	for (size_t i = 0; i < TOTAL_CHUNKS; ++i)
 	{
-		size_t current_chunk_index = DATA_POOL_START + (i * CHUNK_SIZE);
+		std::uint16_t current_chunk_index = DATA_POOL_START + (i * CHUNK_SIZE);
 
 		DataChunk* current_chunk = reinterpret_cast<DataChunk*>(&data[current_chunk_index]);
 		
-		size_t next_chunk_index = DATA_POOL_START + ((i+1) * CHUNK_SIZE);
+		std::uint16_t next_chunk_index = DATA_POOL_START + ((i+1) * CHUNK_SIZE);
 
 		if (i < TOTAL_CHUNKS - 1)
 		{
@@ -47,13 +50,13 @@ void init_chunks() {
 		}
 		else
 		{
-			current_chunk->next = NULL;
+			current_chunk->next = 0;
 		}
 
-		std::cout << "Chunk: " << i << " created. It points to: " << current_chunk->next << std::endl;
+		//std::cout << "Chunk: " << i << " created. It points to: " << current_chunk->next << std::endl;
 	}
 
-	std::cout << "Memory allocated. " << "Free list head: " << free_list_head << std::endl;
+	//std::cout << "Memory allocated. " << "Free list head: " << free_list_head << std::endl;
 }
 
 // Handle out of memory exceptions
@@ -67,22 +70,28 @@ void on_illegal_operation() {
 
 // Creates a FIFO byte queue, returning a handle to it.
 Q* create_queue() {
-	size_t index = free_list_head;
-	Queue* sq_ptr = reinterpret_cast<Queue*>(&data[index]);
-	DataChunk* current_chunk = reinterpret_cast<DataChunk*>(&data[index]);
+	std::uint16_t metadata_id = 0;
+	size_t sq_byte_index = metadata_id * (sizeof(Queue) / sizeof(unsigned char));
+	Queue* sq_ptr = reinterpret_cast<Queue*>(&data[sq_byte_index]);
 
-	sq_ptr->head = index;
-	sq_ptr->tail = sq_ptr->head;
-	sq_ptr->size = 0;
-
-	if (current_chunk->next == NULL) {
+	if (free_list_head == 0) {
 		on_out_of_memory();
-		return nullptr;
 	}
 
-	std::cout << "Queue created at: " << sq_ptr << ". Head at: " << sq_ptr->head << ". Tail at: " << sq_ptr->tail << ". Size: " << sq_ptr->size << std::endl;
+	size_t chunk_index = free_list_head;
+
+	DataChunk* current_chunk = reinterpret_cast<DataChunk*>(&data[chunk_index]);
 
 	free_list_head = current_chunk->next;
+
+	current_chunk->next = 0;
+
+	sq_ptr->head = chunk_index;
+	sq_ptr->tail = chunk_index;
+	sq_ptr->size = 0;
+	sq_ptr->read_offset = 0;
+
+	std::cout << "Queue created at: " << sq_ptr << ". Head at: " << sq_ptr->head << ". Tail at: " << sq_ptr->tail << ". Size: " << sq_ptr->size << std::endl;
 
 	return reinterpret_cast<Q*>(sq_ptr);
 }
@@ -96,25 +105,39 @@ Q* create_queue() {
 // Adds a new byte to a queue.
 void enqueue_byte(Q* q, unsigned char b) {
 	Queue* sq_ptr = reinterpret_cast<Queue*>(q);
-
-	std::uint16_t sq_byte_count = sq_ptr->size % CHUNK_SIZE;
-
 	DataChunk* current_chunk = reinterpret_cast<DataChunk*>(&data[sq_ptr->tail]);
 
-	if (sq_byte_count == 0 && sq_ptr->size > 0) {
+	std::uint16_t sq_byte_count = sq_ptr->size % (DATA_SIZE);
 
-		size_t index = free_list_head;
-		//free_list_head = ;
+	if (sq_byte_count == 0 && sq_ptr->size > 0) {
+		std::uint16_t new_chunk_index = free_list_head;
+
+		if (new_chunk_index == 0) {
+			on_out_of_memory();
+			return;
+		}
+
+		DataChunk* new_chunk = reinterpret_cast<DataChunk*>(&data[new_chunk_index]);
+
+		free_list_head = new_chunk->next;
+		new_chunk->next = 0;
+
+		current_chunk->next = new_chunk_index;
+
+		sq_ptr->tail = new_chunk_index;
+
+		
+		current_chunk = new_chunk;
 	}
 
 	current_chunk->data[sq_byte_count] = b;
 
 	sq_ptr->size++;
 
-	//std::cout << "Enqueued '" << (int)b << "' into queue at " << absolute_index << ". New size: " << sq_ptr->size << std::endl;
+	std::cout << "Enqueued '" << (int)b << "'. Tail at " << sq_ptr->tail << ". Local offset " << sq_byte_count << ". New size: " << sq_ptr->size << std::endl;
 }
 // Pops the next byte off the FIFO queue.
-/*unsigned char dequeue_byte(Q* q) {
+unsigned char dequeue_byte(Q* q) {
 	Queue* sq_ptr = reinterpret_cast<Queue*>(q);
 
 	if (sq_ptr->size == 0) {
@@ -122,43 +145,58 @@ void enqueue_byte(Q* q, unsigned char b) {
 		return 0;
 	}
 
-	unsigned char dequeued_byte = data[sq_ptr->start_id];
+	DataChunk* head_chunk = reinterpret_cast<DataChunk*>(&data[sq_ptr->head]);
 
-	sq_ptr->start_id++;
+
+	unsigned char dequeued_byte = head_chunk->data[sq_ptr->read_offset];
+
+	sq_ptr->read_offset++;
+
+	if (sq_ptr->read_offset == DATA_SIZE) {
+
+		std::uint16_t old_chunk_index = sq_ptr->head;
+
+		sq_ptr->head = head_chunk->next;
+
+		head_chunk->next = free_list_head;
+		free_list_head = old_chunk_index;
+
+		sq_ptr->read_offset = 0;
+	}
+
 	sq_ptr->size--;
-
 	return dequeued_byte;
-}*/
+}
 
 int main()
 {	
 	init_chunks();
 
 
-	/*for (size_t i = 0; i < 63; i++)
+	for (size_t i = 0; i < 17; i++)
 	{
 		Q* q = create_queue();
-		for (size_t j = 0; j < 12; j++)
+		for (size_t j = 0; j < 13; j++)
 		{
 			enqueue_byte(q, j);
 		}
-		for (size_t j = 0; j < 12; j++)
+		for (size_t j = 0; j < 15; j++)
 		{
 			printf("%d", dequeue_byte(q));
 			printf("\n");
 		}
-		if (i == 12) {
+		/*if (i == 12) {
 			destroy_queue(q);
-		}
-	}*/
+		}*/
+	}
 
-	Q* q0 = create_queue();
+	/*Q* q0 = create_queue();
 	Q* q1 = create_queue();
 
 	std::cout << "Free list head: " << free_list_head << std::endl;
 
 	enqueue_byte(q0, 0);
-	enqueue_byte(q0, 1);
+	enqueue_byte(q0, 1);*/
 
 	/*
 	Q* q0 = create_queue();
